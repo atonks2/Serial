b671bbb0585263464f8cdb4eb1e4b6224452cc72
atonks2 <atonks2@gmail.com>
2017-02-09 13:34:55 -0700

    Formatting improvements

--- serial.cpp
+++ serial.cpp
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2017 Alan Tonks
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include "serial.h"
 #include <errno.h>
 #include <stdio.h>
@@ -9,26 +33,26 @@
 #include <fcntl.h>
 
 Serial::Serial() {
-        PORT = "/dev/ttyUSB0"; /* A default to use if no port is specified */
-        if (setBaud(4800) == 0) BAUDRATE = 4800; /* Use 4800 baud if no rate is specified */
-        isOpen = false; /* Port isn't open yet */
-        isCanonical = true;
+	PORT = "/dev/ttyUSB0"; /* A default to use if no port is specified */
+	if (setBaud(4800) == 0) BAUDRATE = 4800; /* Use 4800 baud if no rate is specified */
+	isOpen = false; /* Port isn't open yet */
+	isCanonical = true;
 	init();
 }
- 
+
 Serial::Serial(speed_t baud, std::string port) /* Recommended constructor */
 {
-    if (setBaud(baud) == 0) BAUDRATE = baud; /* Check for valid baudrate and set the flag if it is valid */
-    isOpen = false; /* Port isn't open yet */
-    isCanonical = true;
-    struct stat stat_buf; /* Used in checking if specified port exists */
-    if (stat(port.c_str(), &stat_buf) == 0) /* Make sure the specified port exists */
-        PORT = port; // port is valid
-    else {
-        std::cout << "Device not found." << std::endl; /* Port not found */
-        exit(-1);
-    }
-    init();
+	if (setBaud(baud) == 0) BAUDRATE = baud; /* Check for valid baudrate and set the flag if it is valid */
+	isOpen = false; /* Port isn't open yet */
+	isCanonical = true;
+	struct stat stat_buf; /* Used in checking if specified port exists */
+	if (stat(port.c_str(), &stat_buf) == 0) /* Make sure the specified port exists */
+		PORT = port; // port is valid
+	else {
+		std::cout << "Device not found." << std::endl; /* Port not found */
+		exit(-1);
+	}
+	init();
 }
 
 Serial::Serial(speed_t baud, std::string port, bool canon)
@@ -43,80 +67,80 @@ Serial::Serial(speed_t baud, std::string port, bool canon)
 		exit(-1);
 	}
 	init();
-} 
+}
 
 void Serial::init() /* Open and configure the port */
 {
-    /* open port for read and write, not controlling */
-    dev_fd = open(PORT.c_str(), O_RDWR | O_NOCTTY); /* open port for read/write, not controlling */
-    if (dev_fd < 0) {
-        perror("Failed to open device: ");
-        exit(-1);
-    }
-    else if (dev_fd >= 0)
-        isOpen = true;
- 
-    tcgetattr(dev_fd, &old_config); /* save current port settings */
- 
-    // define configuration vars for termios struct
-    memset(&new_config, 0, sizeof(new_config)); /* clear mem first */
-    tcgetattr(dev_fd, &new_config); /* Per GNU C, get current config and modify the flags you need. Don't start from scratch */
-    /*
-       BAUDRATE: Integer multiple of 2400
-       CRTSCTS: Hardware flow control
-       CS8: 8N1
-       CLOCAL: No modem control. (local device)
-       CREAD: Receive chars
-    */
-    new_config.c_cflag |= (BAUDRATE | CS8 | CLOCAL | HUPCL | CREAD);
-    /*
-       IGNPAR: Ignore parity errors
-    */
-    new_config.c_iflag |= IGNPAR;
-    new_config.c_oflag = 0; /* use raw output */
-    if (isCanonical)  new_config.c_lflag |= ICANON; /* Canonical input */
-    else {
-        /* Configure for non-canonical mode */
-        new_config.c_lflag &= ~(ICANON | ECHO);
-        new_config.c_cc[VMIN] = 1;
-        new_config.c_cc[VTIME] = 0;
-    }
-    tcflush(dev_fd, TCIFLUSH); /* flush serial line */
-    applyNewConfig();
+	/* open port for read and write, not controlling */
+	dev_fd = open(PORT.c_str(), O_RDWR | O_NOCTTY); /* open port for read/write, not controlling */
+	if (dev_fd < 0) {
+		perror("Failed to open device: ");
+		exit(-1);
+	}
+	else if (dev_fd >= 0)
+		isOpen = true;
+
+	tcgetattr(dev_fd, &old_config); /* save current port settings */
+
+	// define configuration vars for termios struct
+	memset(&new_config, 0, sizeof(new_config)); /* clear mem first */
+	tcgetattr(dev_fd, &new_config); /* Per GNU C, get current config and modify the flags you need. Don't start from scratch */
+	/*
+	   BAUDRATE: Integer multiple of 2400
+	   CRTSCTS: Hardware flow control
+	   CS8: 8N1
+	   CLOCAL: No modem control. (local device)
+	   CREAD: Receive chars
+	*/
+	new_config.c_cflag |= (BAUDRATE | CS8 | CLOCAL | HUPCL | CREAD);
+	/*
+	   IGNPAR: Ignore parity errors
+	*/
+	new_config.c_iflag |= IGNPAR;
+	new_config.c_oflag = 0; /* use raw output */
+	if (isCanonical)  new_config.c_lflag |= ICANON; /* Canonical input */
+	else {
+		/* Configure for non-canonical mode */
+		new_config.c_lflag &= ~(ICANON | ECHO);
+		new_config.c_cc[VMIN] = 1;
+		new_config.c_cc[VTIME] = 0;
+	}
+	tcflush(dev_fd, TCIFLUSH); /* flush serial line */
+	applyNewConfig();
 }
- 
+
 int Serial::setBaud(speed_t baud)
 {
 	if (!isOpen) return -1;
 	int status = -1;
-	switch(baud) {
-		case 2400:
-			status = cfsetspeed(&new_config,B2400);
-			break;
-		case 4800:
-			status = cfsetspeed(&new_config,B4800);
-			break;
-		case 9600:
-			status = cfsetspeed(&new_config,B9600);
-			break;
-		case 19200:
-			status = cfsetspeed(&new_config,B19200);
-			break;
-		case 38400:
-			status = cfsetspeed(&new_config,B38400);
-			break;
-		case 57600:
-			status = cfsetspeed(&new_config,B57600);
-			break;
-		case 115200:
-			status = cfsetspeed(&new_config,B115200);
-			break;
-		case 230400:
-			status = cfsetspeed(&new_config,B230400);
-			break;
-		default: 
-			std::cout << "Invalid baudrate requested.\n";
-			return -1;
+	switch (baud) {
+	case 2400:
+		status = cfsetspeed(&new_config, B2400);
+		break;
+	case 4800:
+		status = cfsetspeed(&new_config, B4800);
+		break;
+	case 9600:
+		status = cfsetspeed(&new_config, B9600);
+		break;
+	case 19200:
+		status = cfsetspeed(&new_config, B19200);
+		break;
+	case 38400:
+		status = cfsetspeed(&new_config, B38400);
+		break;
+	case 57600:
+		status = cfsetspeed(&new_config, B57600);
+		break;
+	case 115200:
+		status = cfsetspeed(&new_config, B115200);
+		break;
+	case 230400:
+		status = cfsetspeed(&new_config, B230400);
+		break;
+	default:
+		std::cout << "Invalid baudrate requested.\n";
+		return -1;
 	}
 	if (status < 0) {
 		perror("Failed to set requested baudrate: ");
@@ -133,60 +157,60 @@ int Serial::applyNewConfig()
 	}
 	else return -1;
 }
- 
-/* return current line speed. 
+
+/* return current line speed.
 *  Line speeds are defined as hexidecimal
-*  constants. cfgetispeed() returns 
-*  speed_t (unsigned int). 
+*  constants. cfgetispeed() returns
+*  speed_t (unsigned int).
 *   12 = 4800, 13 = 9600, etc
 */
 speed_t Serial::getBaud()
 {
-    if (isOpen) return cfgetispeed(&new_config);
-    else return 1;
+	if (isOpen) return cfgetispeed(&new_config);
+	else return 1;
 }
- 
+
 /* return current port coniguration flags */
 termios Serial::getConfig()
 {
-    return new_config;
+	return new_config;
 }
- 
+
 int Serial::serialRead()
 {
-    if (!isOpen) return -1;
-    int buf_size = 255; // 82 is longest NMEA Sentence
-    char buf[buf_size];
-    if (tcflush(dev_fd, TCIOFLUSH < 0)) perror("Could not flush line: ");
-    bytes_received = read(dev_fd, buf, buf_size);
-    if (bytes_received < 0) perror("Read failed: ");
-    else buf[bytes_received] = '\0'; /* Null terminate the string */
-    data.assign(buf); /* store data as a c++ string */
-    return bytes_received;
+	if (!isOpen) return -1;
+	int buf_size = 255; // 82 is longest NMEA Sentence
+	char buf[buf_size];
+	if (tcflush(dev_fd, TCIOFLUSH < 0)) perror("Could not flush line: ");
+	bytes_received = read(dev_fd, buf, buf_size);
+	if (bytes_received < 0) perror("Read failed: ");
+	else buf[bytes_received] = '\0'; /* Null terminate the string */
+	data.assign(buf); /* store data as a c++ string */
+	return bytes_received;
 }
 
 int Serial::serialRead(int bytes)
 {
-    if (!isOpen) return -1;
-    int buf_size = 255;
-    char buf[buf_size];
-    if (tcflush(dev_fd, TCIOFLUSH <0)) perror("Could not flush line: ");
-    if (bytes > buf_size) bytes = buf_size;
-    bytes_received = read(dev_fd, buf, bytes);
-    if (bytes_received < 0) perror("Read failed: ");
-    else buf[bytes_received] = '\0';
-    data.assign(buf);
-    return bytes_received;
+	if (!isOpen) return -1;
+	int buf_size = 255;
+	char buf[buf_size];
+	if (tcflush(dev_fd, TCIOFLUSH < 0)) perror("Could not flush line: ");
+	if (bytes > buf_size) bytes = buf_size;
+	bytes_received = read(dev_fd, buf, bytes);
+	if (bytes_received < 0) perror("Read failed: ");
+	else buf[bytes_received] = '\0';
+	data.assign(buf);
+	return bytes_received;
 }
- 
+
 std::string Serial::getData()
 {
-    if (isOpen) return data;
-    else return "Open serial port first!\n";
+	if (isOpen) return data;
+	else return "Open serial port first!\n";
 }
- 
+
 Serial::~Serial()
 {
-    tcsetattr(dev_fd, TCSANOW, &old_config); /* Leave port how we found it. */
-    close(dev_fd); /* close the port */
+	tcsetattr(dev_fd, TCSANOW, &old_config); /* Leave port how we found it. */
+	close(dev_fd); /* close the port */
 }
--- serial.h
+++ serial.h
@@ -1,34 +1,62 @@
+/*
+MIT License
+
+Copyright (c) 2017 Alan Tonks
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #ifndef SERAIL_H
 #define SERIAL_H
- 
+
 #include <termios.h>
 #include <iostream>
 #include <string.h>
- 
+
 class Serial
 {
 public:
-        Serial();
+	// Default constructor assumes canonical input,
+	// PORT = /dev/ttyUSB0 and BAUDRATE = 4800
+	Serial();
+
+
 	Serial(speed_t baud, std::string port); /* Assume canonical input */
 	Serial(speed_t baud, std::string port, bool canon);
 
-        int setBaud(speed_t baud);
+	int setBaud(speed_t baud);
 	int applyNewConfig();
-        int serialRead(); /* Canonical read (line) */
+	int serialRead(); /* Canonical read (line) */
 	int serialRead(int bytes); /* non-canonical read */
 
 	speed_t getBaud();
-        termios getConfig();
-        std::string getData();
-         
-        ~Serial();
+	termios getConfig();
+	std::string getData();
+
+	~Serial();
 private:
-        void init();
-        struct termios old_config, new_config;
-        int dev_fd, bytes_received;
-        speed_t BAUDRATE;
-        std::string PORT, data;
-        bool isOpen, isCanonical;
+	void init();
+	struct termios old_config, new_config;
+	int dev_fd, bytes_received;
+	speed_t BAUDRATE;
+	std::string PORT, data;
+	bool isOpen, isCanonical;
 };
- 
-#endif
+
+#endif // SERIAL_H
